b7b3b9f1e654ca6441e8da9aad4a4ec5ab3acbcb
BRIOAGENTIC <tjlearner15297@gmail.com>
2026-08-22 10:55:05 +0530

    Replace GoogleCSEClient with TavilyClient

--- app/search/__init__.py
+++ app/search/__init__.py
@@ -1,4 +1,4 @@
-from .google_cse import GoogleCSEClient, cse_client
+from .tavily_client import TavilyClient, tavily_client
 from .query_builder import build_queries
 
-__all__ = ["GoogleCSEClient", "cse_client", "build_queries"]
+__all__ = ["TavilyClient", "tavily_client", "build_queries"]
